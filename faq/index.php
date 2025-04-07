a9b68861c8ee3cadc2fc470ff21645042741d8a3
xmartina <martinsprince126@gmail.com>
2025-04-07 17:31:55 +0100

    d

--- faq/index.php
+++ faq/index.php
@@ -1,1 +1,298 @@
 <?php
+$page_name = 'FAQ';
+include_once '../layout/header.php';
+include_once '../layout/breadcrumb.php';
+?>
+
+<section id="/faq" class="pt-100 pb-100 section--bg">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xxl-5 col-lg-7 wow fadeInLeft" data-wow-duration="0.5s" data-wow-delay="0.3s">
+                <div class="section-header text-center">
+                    <h2 class="section-title">Frequently Asked Questions</h2>
+                    <p class="mt-2">Though we have provided lots of information about us and how we serve what is our
+                        working process our terms and conditions our policies etc.</p>
+                </div>
+            </div>
+        </div>
+        <div class="accordion custom--accordion" id="/faqAccordion">
+            <div class="row gy-4 justify-content-center">
+                <div class="col-lg-6 wow fadeInRight " data-wow-duration="0.5s" data-wow-delay="0.3s">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="h-71">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#c-71" aria-expanded="false" aria-controls="c-71">
+                                Is opening an account is free?
+                            </button>
+                        </h2>
+                        <div id="c-71" class="accordion-collapse collapse" aria-labelledby="h-71"
+                             data-bs-parent="#faqAccordion">
+                            <div class="accordion-body">
+                                <p>Yes, we don&#039;t take any fees for opening an account.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 wow fadeInRight " data-wow-duration="0.5s" data-wow-delay="0.3s">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="h-72">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#c-72" aria-expanded="false" aria-controls="c-72">
+                                Is it possible to send money from Greystone Union to another bank?
+                            </button>
+                        </h2>
+                        <div id="c-72" class="accordion-collapse collapse" aria-labelledby="h-72"
+                             data-bs-parent="#faqAccordion">
+                            <div class="accordion-body">
+                                <p>Yes, you can send money from Greystone Union to another bank?</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 wow fadeInRight " data-wow-duration="0.5s" data-wow-delay="0.3s">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="h-88">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#c-88" aria-expanded="false" aria-controls="c-88">
+                                How to open an account?
+                            </button>
+                        </h2>
+                        <div id="c-88" class="accordion-collapse collapse" aria-labelledby="h-88"
+                             data-bs-parent="#faqAccordion">
+                            <div class="accordion-body">
+                                <p>Get the registration form by clicking on the Sign Up button on the top bar. Provide
+                                    all information and click on the Sign Up button.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 wow fadeInRight " data-wow-duration="0.5s" data-wow-delay="0.3s">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="h-89">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#c-89" aria-expanded="false" aria-controls="c-89">
+                                Does Greystone Union share our information for advertisement?
+                            </button>
+                        </h2>
+                        <div id="c-89" class="accordion-collapse collapse" aria-labelledby="h-89"
+                             data-bs-parent="#faqAccordion">
+                            <div class="accordion-body">
+                                <p>No, we don&#039;t provide our account holder&#039;s information to any third-party
+                                    organization.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 wow fadeInRight " data-wow-duration="0.5s" data-wow-delay="0.3s">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="h-115">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#c-115" aria-expanded="false" aria-controls="c-115">
+                                How to take a loan?
+                            </button>
+                        </h2>
+                        <div id="c-115" class="accordion-collapse collapse" aria-labelledby="h-115"
+                             data-bs-parent="#faqAccordion">
+                            <div class="accordion-body">
+                                <p>We have several loan plans. Choose the best plan suitable for you and just click on
+                                    the Apply Now button and put the amount.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 wow fadeInRight " data-wow-duration="0.5s" data-wow-delay="0.3s">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="h-122">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#c-122" aria-expanded="false" aria-controls="c-122">
+                                How to open a FDR
+                            </button>
+                        </h2>
+                        <div id="c-122" class="accordion-collapse collapse" aria-labelledby="h-122"
+                             data-bs-parent="#faqAccordion">
+                            <div class="accordion-body">
+                                <p>We have several FDR plans. Choose the best plan suitable for you and just click on
+                                    the Apply Now button and put the amount.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="/services" class="service-section position-relative z-index-2 pt-100 pb-100">
+    <div class="top-wave">
+        <img src="/assets/templates/indigo_fusion/images/elements/white-wave-2.png" alt="wave image">
+    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xxl-7 col-xl-8 col-lg-10">
+                <div class="section-header text-center">
+                    <div class="section-top-title border-left text--base">Our Services</div>
+                    <h2 class="section-title">Banking Made for You</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center gy-4">
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.3s">
+                <div class="service-card rounded-3">
+                    <div class="service-card__icon rounded-2 text--base">
+                        <i class="las la-exchange-alt"></i></div>
+                    <div class="service-card__content">
+                        <h3 class="title">Personal Banking</h3>
+                        <p class="mt-2">Manage everyday transactions with ease.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.3s">
+                <div class="service-card rounded-3">
+                    <div class="service-card__icon rounded-2 text--base">
+                        <i class="fas fa-credit-card"></i></div>
+                    <div class="service-card__content">
+                        <h3 class="title">Business Banking</h3>
+                        <p class="mt-2">Financial solutions to support and grow your business.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.3s">
+                <div class="service-card rounded-3">
+                    <div class="service-card__icon rounded-2 text--base">
+                        <i class="fas fa-money-check-alt"></i></div>
+                    <div class="service-card__content">
+                        <h3 class="title">Palm Trust Pay</h3>
+                        <p class="mt-2">Quick and secure online payments.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="testimonial-section pt-100 pb-100">
+    <div class="container">
+        <div class="testimonial-slider wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.3s">
+            <div class="single-slide">
+                <div class="testimonial-item">
+                    <div class="ratings">
+                        <i class="la la-star"></i><i class="la la-star"></i><i class="la la-star"></i><i
+                            class="la la-star"></i><i class="la la-star"></i></div>
+                    <p class="text-white mt-2">Their 24/7 customer service is amazing! Every issue I had was resolved
+                        quickly and professionally.</p>
+                    <div class="d-flex align-items-center mt-4">
+                        <h4 class="name text-white">Adam Gilly</h4>
+                        <span class="designation text-white-50 ms-3 fs--14px">
+                                    Forte Wayne
+                                </span>
+                    </div>
+                </div>
+            </div>
+            <div class="single-slide">
+                <div class="testimonial-item">
+                    <div class="ratings">
+                        <i class="la la-star"></i><i class="la la-star"></i><i class="la la-star"></i><i
+                            class="la la-star"></i><i class="la la-star"></i></div>
+                    <p class="text-white mt-2">Getting a loan was super easy, with flexible repayment options. Greystone
+                        Union truly cares about its customers.</p>
+                    <div class="d-flex align-items-center mt-4">
+                        <h4 class="name text-white">Maria Ahsan</h4>
+                        <span class="designation text-white-50 ms-3 fs--14px">
+                                    Seattle
+                                </span>
+                    </div>
+                </div>
+            </div>
+            <div class="single-slide">
+                <div class="testimonial-item">
+                    <div class="ratings">
+                        <i class="la la-star"></i><i class="la la-star"></i><i class="la la-star"></i><i
+                            class="la la-star"></i><i class="la la-star"></i></div>
+                    <p class="text-white mt-2">I love the security features, especially the OTP and 2FA options. I feel
+                        confident knowing my funds are protected.</p>
+                    <div class="d-flex align-items-center mt-4">
+                        <h4 class="name text-white">Michel Johnson</h4>
+                        <span class="designation text-white-50 ms-3 fs--14px">
+                                    Denver
+                                </span>
+                    </div>
+                </div>
+            </div>
+            <div class="single-slide">
+                <div class="testimonial-item">
+                    <div class="ratings">
+                        <i class="la la-star"></i><i class="la la-star"></i><i class="la la-star"></i><i
+                            class="la la-star"></i><i class="la la-star"></i></div>
+                    <p class="text-white mt-2">Greystone Union has completely transformed how I manage my finances. The
+                        app is user-friendly, and transactions are instant. Highly recommend!</p>
+                    <div class="d-flex align-items-center mt-4">
+                        <h4 class="name text-white">John Smith</h4>
+                        <span class="designation text-white-50 ms-3 fs--14px">
+                                    Chicago
+                                </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="overview-section pt-100 pb-50">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-8 col-lg-10">
+                <div class="section-header text-center">
+                    <h2 class="section-title text-white">Global Banking, Anytime, Anywhere</h2>
+                    <p class="mt-3 text-white">Our seamless banking services are available worldwide, ensuring secure
+                        and hassle-free transactions.</p>
+                </div>
+            </div>
+        </div>
+        <div class="overview-area wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.3s">
+            <div class="row gy-4 justify-content-center">
+                <div class="col-lg-3 col-6">
+                    <div class="overview-card">
+                        <div class="overview-card__icon">
+                            <i class="las la-user-circle"></i></div>
+                        <div class="overview-card__content">
+                            <h4 class="couter-number">24M</h4>
+                            <p>Account Holders</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-6">
+                    <div class="overview-card">
+                        <div class="overview-card__icon">
+                            <i class="las la-coins"></i></div>
+                        <div class="overview-card__content">
+                            <h4 class="couter-number">3B</h4>
+                            <p>Total Transaction</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-6">
+                    <div class="overview-card">
+                        <div class="overview-card__icon">
+                            <i class="las la-project-diagram"></i></div>
+                        <div class="overview-card__content">
+                            <h4 class="couter-number">120</h4>
+                            <p>Total Branches</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-6">
+                    <div class="overview-card">
+                        <div class="overview-card__icon">
+                            <i class="las la-globe-africa"></i></div>
+                        <div class="overview-card__content">
+                            <h4 class="couter-number">240+</h4>
+                            <p>Countries We Serve</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php
+include_once '../layout/footer.php';
+?>
