a966452440bf0c5bbb94c63b1d60c20c6334c58e
xmartina <martinsprince126@gmail.com>
2025-04-07 17:02:50 +0100

    d

--- layout/footer.php
+++ layout/footer.php
@@ -150,7 +150,7 @@
         <div class="footer__bottom">
             <div class="row gy-4 align-items-center">
                 <div class="col-lg-3 col-sm-6 order-lg-1 text-sm-start order-1 text-center">
-                    <a class="footer-logo" href="/"><img src="assets/images/logoIcon/logo.png" alt="Logo"></a>
+                    <a class="footer-logo" href="/"><img src="assets/images/added/logo.png" alt="Logo"></a>
                 </div>
 
                 <div class="col-lg-9 col-sm-6 order-lg-3 text-sm-end order-2 text-center">
